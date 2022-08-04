8834f8375050ed2c51027b71deff26f16aa9f511
PoseidonLauSens <gaietan.renault@epfl.ch>
2022-08-04 19:15:12 +0200

    Merge

--- autofocus/test.py
+++ autofocus/test.py
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import logging
 import sangaboard
 
@@ -15,44 +13,4 @@ sangaboard.Sangaboard.port_settings["baudrate"] = 9_600
 sb = sangaboard.Sangaboard("COM3")
 sb.move_rel([1000,0,0])
 sb.position
-=======
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
-import logging
-import sangaboard
-
-# Print logs
-logging.basicConfig(level=logging.DEBUG)
-
-class Sangaboard2(sangaboard.Sangaboard):
-    def test_communications(self):
-        return True
-
-sangaboard.Sangaboard.port_settings["baudrate"] = 9_600
-
-sb = sangaboard.Sangaboard("COM3")
-sb.move_rel([1000,0,0])
-sb.position
-=======
-import logging
-import sangaboard
-
-# Print logs
-logging.basicConfig(level=logging.DEBUG)
-
-class Sangaboard2(sangaboard.Sangaboard):
-    def test_communications(self):
-        return True
-
-sangaboard.Sangaboard.port_settings["baudrate"] = 9_600
-
-sb = sangaboard.Sangaboard("COM3")
-sb.move_rel([1000,0,0])
-sb.position
->>>>>>> Stashed changes
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 exit()
